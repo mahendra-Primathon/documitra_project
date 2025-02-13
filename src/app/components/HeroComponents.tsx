893d84e69c58ba29d0e3ab535697d3a13afdacf3
mahendra-primathon <mahendrapuniya92@gmail.com>
2025-02-13 18:48:20 +0530

    firebase is done  / ad other data is done

--- src/app/components/HeroComponents.tsx
+++ src/app/components/HeroComponents.tsx
@@ -70,6 +70,7 @@ export const MoreDropdown = ({ options, selectedDoc, onSelect, isOpen, setIsOpen
   useClickOutside(dropdownRef, () => setIsOpen(false));
 
   const [position, setPosition] = useState({ top: 0, left: 0, width: 0 });
+  const [selectedOption, setSelectedOption] = useState(selectedDoc); // Initialize with selectedDoc
 
   useEffect(() => {
     if (isOpen && buttonRef.current) {
@@ -82,11 +83,11 @@ export const MoreDropdown = ({ options, selectedDoc, onSelect, isOpen, setIsOpen
     }
   }, [isOpen]);
 
-  // Main document types that should keep "More" label
-  const mainDocumentTypes = ["OCI", "VISA", "PASSPORT", "PANCARD"];
-
-  // Determine button label
-  const buttonLabel = mainDocumentTypes.includes(selectedDoc?.name) ? "More" : selectedDoc?.name || "More";
+  const handleSelect = (option) => {
+    setSelectedOption(option); // Update selected option
+    onSelect(option);
+    setIsOpen(false);
+  };
 
   return (
     <>
@@ -95,16 +96,11 @@ export const MoreDropdown = ({ options, selectedDoc, onSelect, isOpen, setIsOpen
         onClick={() => setIsOpen(!isOpen)}
         className="flex items-center gap-2 px-4 py-2 text-gray-600"
       >
-        <span>More</span>
-        {/* <span>{buttonLabel}</span> */}
+        <span>{selectedOption ? selectedOption.name : "More"}</span> {/* Display selected option */}
         {isOpen ? (
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clipRule="evenodd" />
-          </svg>
+          <ChevronUp className="w-5 h-5" />
         ) : (
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
-          </svg>
+          <ChevronDown className="w-5 h-5" />
         )}
       </button>
 
@@ -125,11 +121,8 @@ export const MoreDropdown = ({ options, selectedDoc, onSelect, isOpen, setIsOpen
               {options?.map((option, index) => (
                 <button
                   key={index}
-                  onClick={() => {
-                    onSelect(option);
-                    setIsOpen(false);
-                  }}
-                  className="flex  gap-2 px-1 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+                  onClick={() => handleSelect(option)} // Use handleSelect function
+                  className="flex gap-2 px-1 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
                 >
                   {option.icon}
                   <span>{option.name}</span>
