9f2a9a68dc8fe54f2f9b7c0b72d4a2bc77da2936
mahendra-primathon <mahendrapuniya92@gmail.com>
2025-03-17 17:00:23 +0530

    Enhance ViewFormData component with loading and error handling; add delete functionality for form entries

--- src/app/components/DashboardMyPackages.tsx
+++ src/app/components/DashboardMyPackages.tsx
@@ -67,7 +67,7 @@ const MyPackages: React.FC = () => {
   }
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 bg-secondary ">
       <h1 className="text-2xl font-bold mb-8">My Packages</h1>
       
       {packages.map((pkg) => (
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import Header from '../components/Header';
-import { Suspense } from 'react';
-import DashboardMyPackages from '../components/DashboardMyPackages';
+import React from "react";
+import Header from "../components/Header";
+import { Suspense } from "react";
+import DashboardMyPackages from "../components/DashboardMyPackages";
 
-export default function Dashboard(){
-    return (
-        <div className="min-h-screen bg-secondary ">
-          <div className="max-w-7xl mx-auto">
-            <Header />
-            <Suspense fallback={<div>Loading...</div>}>
-              <DashboardMyPackages/>
-            </Suspense>
-          </div>
+export default function Dashboard() {
+  return (
+    <div className=" ">
+      <div className="">
+        <Header />
+        <div className="bg-secondary">
+          <Suspense fallback={<div>Loading...</div>}>
+            <DashboardMyPackages />
+          </Suspense>
         </div>
-    )
-};
+      </div>
+    </div>
+  );
+}
