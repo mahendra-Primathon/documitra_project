7036396460daba1353fad24c57f8c4bb64625254
mahendra-primathon <mahendrapuniya92@gmail.com>
2025-03-20 10:32:31 +0530

    Refactor blog page component: rename to PascalCase and clean up JSX formatting

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,18 +1,16 @@
-"use client";
 import React from 'react'
 import BlogComingSoon from '../constants/BlogComingSoon'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
-const page = () => {
+const Page = () => {
   return (
     <div>
-        <Header/>
-       
-       <BlogComingSoon/>
-       <Footer/>
+        <Header />
+        <BlogComingSoon />
+        <Footer />
     </div>
   )
 }
 
-export default page
+export default Page;
